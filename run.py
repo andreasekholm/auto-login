364f80851a61fbc5f2bc4a917b9f47522593a69a
andreasekholm <me@andreasekholm.com>
2014-08-02 13:45:51 -0700

    qt wrap

--- run.py
+++ run.py
@@ -1,17 +1,53 @@
 #!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+"""
+Very basic auth for wifi portal.
+
+Written by: https://github.com/andreasekholm
+
+Thanks to: https://github.com/bouncynudibranch
+
+TODO:
+    * Retrying after x amount of time
+    * Force login page to extend lease
+    * If that doesn't work, call for relog after set amount of time
+"""
 
 import requests
- 
+import sys
+import os
+from PyQt4 import QtGui, QtCore
+
 url = 'http://192.168.3.1:8000/?redirurl=http%3A%2F%2Fwww.google.com'
 data = {
         'auth_user': 'cartel',
         'auth_pass': 'cartel',
         'redirurl': 'http://www.google.com',
         'accept': 'Continue'
-}
- 
-r = requests.post(url, data=data)
-print r.status_code
+        }
 
-#@retry(wait_fixed=0)
+def try_auth(url, data):
+    r = requests.post(url, data=data)
+    return r.status_code
 
+
+class LoginQt(QtGui.QMainWindow):
+    def __init__(self):
+        super(LoginQt, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        button = QtGui.QPushButton('Login to Cartel', self)
+        button.setToolTip('Just push and enjoy free WiFi')
+        button.resize(button.sizeHint())
+        button.clicked.connect(try_auth)
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ulf = LoginQt()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
