1d1d2bb23a2e2dfac0a80898043b12a2f7d8158a
andreasekholm <me@andreasekholm.com>
2014-08-01 23:59:50 -0700

    where it finally works

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
 import requests
+ 
+url = 'http://192.168.3.1:8000/?redirurl=http%3A%2F%2Fwww.google.com'
+data = {
+        'auth_user': 'cartel',
+        'auth_pass': 'cartel',
+        'redirurl': 'http://www.google.com',
+        'accept': 'Continue'
+}
+ 
+r = requests.post(url, data=data)
+print r.status_code
 
-# set login parameters and url
-
-payload = {'auth_pass': 'cartel'}
-
-url = 'http://192.168.3.1:8000/?redirurl=http%3A%2F%2Fgoogle.com%2F'
-
-r = requests.get(url, params=payload)
-
-print(r, url)
+@retry(wait_fixed=2000000)
 
